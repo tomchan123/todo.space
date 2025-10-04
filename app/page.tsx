7c2a7359239f0c7045675d8a5d151f2cbe81a0e9
Tom Chan <>
2025-10-04 23:41:57 +0930

    feat: add custom glitch effect

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
       <main>
         <h1>
           Welcome to{" "}
-          <span className="italic animate-pulse font-mono">todo.space</span>
+          <span className="italic font-mono before:[clip-path:rect(0_100%_14px_0)] before:-left-1 before:text-shadow-lg before:text-shadow-blue-500 before:content-['todo.space'] before:absolute before:bg-background after:[clip-path:rect(70%_100%_100%_0%)] after:left-1 after:text-shadow-lg after:text-shadow-blue-500 after:content-['todo.space'] after:absolute after:bg-background relative text-3xl">
+            todo.space
+          </span>
         </h1>
       </main>
     </div>
