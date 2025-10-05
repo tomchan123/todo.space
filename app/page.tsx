1b903ae43c89c33f6bc7b01b9af6d9405bc13a8b
Tom Chan <>
2025-10-05 22:34:56 +1030

    feat: add permanent redirect to welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import Logo from "@/components/Logo";
 
 export default function Home() {
+  const router = useRouter();
+
+  /** TODO: check if user already has an account before redirecting */
+  useEffect(() => {
+    router.replace("/welcome");
+  }, [router.replace]);
+
   return (
     <div className="flex flex-col justify-center items-center h-full gap-2">
       <Logo />
