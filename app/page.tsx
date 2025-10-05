07d348ca0ca36efa58889d355435c626bbff9592
Tom Chan <>
2025-10-05 22:25:42 +1030

    chore: update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
-import { Button } from "@/components/Button";
+import Logo from "@/components/Logo";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-full">
+    <div className="flex flex-col justify-center items-center h-full gap-2">
+      <Logo />
       <main>
-        <h1>
-          Welcome to{" "}
-          <span className="italic font-mono before:[clip-path:rect(0_100%_14px_0)] before:-left-1 before:text-shadow-lg before:text-shadow-blue-500 before:content-['todo.space'] before:absolute before:bg-background after:[clip-path:rect(70%_100%_100%_0%)] after:left-1 after:text-shadow-lg after:text-shadow-blue-500 after:content-['todo.space'] after:absolute after:bg-background relative text-3xl">
-            todo.space
-          </span>
-        </h1>
-        <Button>Click Me</Button>
+        <p>Home Page</p>
       </main>
     </div>
   );
